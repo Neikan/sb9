78e3e1eaf164ab6202699b49b70f0e0bc9210faf
Neikan <sladkov.e.m@outlook.com>
2023-01-21 13:16:51 +0300

    feat(project): add consts

---
+++ lib/consts/keys.dart
@@ -0,0 +1,15 @@
+const String keyTitle = 'title';
+const String keyContent = 'content';
+
+const String keyHotelsGrid = 'hotelsGrid';
+const String keyHotelsList = 'hotelsList';
+
+const String keyHotel = 'hotelId';
+
+const String keyCountry = 'country';
+const String keyCity = 'city';
+const String keyStreet = 'street';
+const String keyRating = 'rating';
+const String keyServices = 'services';
+const String keyFree = 'free';
+const String keyPaid = 'paid';
